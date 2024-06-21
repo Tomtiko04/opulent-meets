1d7da2bdf42524f1e99b4d11438bcbfb5be2c60a
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-06-21 09:53:32 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {Toaster} from "react-hot-toast"
 import Dashboard from './pages/Dashboard';
 import Bookings from "./pages/Bookings";
@@ -32,7 +31,6 @@ export default function App() {
 		<>
 			<DarkModeProvider>
 				<QueryClientProvider client={queryClient}>
-					<ReactQueryDevtools initialIsOpen={false} />
 					<GlobalStyles />
 					<BrowserRouter>
 						<Routes>
